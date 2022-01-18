9d82f116d5eeed5400ab48a825af93faf488fc4b
Shinichi Segawa <tektoh@gmail.com>
2022-01-18 19:32:09 +0900

    パブリック画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  root "posts#index"
+
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
   get "/auth/logout" => "auth0#logout"
