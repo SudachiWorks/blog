c7b196113c879b134df09cabce4bebdaf30abf04
Shinichi Segawa <tektoh@gmail.com>
2022-01-18 21:40:26 +0900

    ログイン

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -3,7 +3,7 @@ class Auth0Controller < ApplicationController
   def callback
     auth_info = request.env["omniauth.auth"]
     session[:userinfo] = auth_info["extra"]["raw_info"]
-    redirect_to root_path
+    redirect_to admin_root_path
   end
 
   def failure
