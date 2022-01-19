f34c5c75980ad279356ac438793350d471904e4d
Shinichi Segawa <tektoh@gmail.com>
2022-01-19 09:24:18 +0900

    navbar

---
+++ test/components/admin/navbar_component_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class Admin::NavbarComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Admin::NavbarComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
---
+++ test/components/admin/navbar_right_links_component_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require "test_helper"
+
+class Admin::NavbarRightLinksComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Admin::NavbarRightLinksComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
